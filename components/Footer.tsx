bac93de87ed5986940c3fbd5973dabb26ffd4edd
Bhushan Rane <eulersidentity2718@gmail.com>
2025-03-27 13:11:09 +0530

    recentred div

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,6 @@ const Footer = () => {
       <div className="relative w-full h-full">
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-steelGray-dark/50"></div>
         <div className="relative z-10">
-          <Socials />
           <div className="text-center my-10">
             <p className="mx-auto pt-5 sm:text-3xl">
               ✷ Link to the  <a
@@ -19,15 +18,17 @@ const Footer = () => {
             </p>
             <small>If you vibe with it, smash that star button! ⭐</small>
           </div>
-          <p className="md:text-base text-sm md:font-normal font-light mt-5 opacity-60">
-            Crafted with ❤ by  <a
-              className="text-purple font-extrabold hover:scale-105 inline-block transition-transform duration-200"
-              target="_blank"
-              href="https://theplumup.com"
-            >
-              Bhushan Rane.
-            </a>
-          </p>
+          <div className="text-center">
+            <p className="md:text-base text-sm md:font-normal font-light mt-5 opacity-60">
+              Crafted with ❤ by  <a
+                className="text-purple font-extrabold hover:scale-105 inline-block transition-transform duration-200"
+                target="_blank"
+                href="https://theplumup.com"
+              >
+                Bhushan Rane.
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
